66df09f5b6ec030762d14137b3ea33c180e8ba20
Myint Myat Thein <myintmyatthein403@gmail.com>
2024-07-06 17:18:12 +0700

    add sonokai adn vim-dadbod

---
+++ lua/plugins/vim-dadbod-ui.lua
@@ -0,0 +1,26 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = {
+      "sql",
+      "mysql",
+      "plsql",
+    }, lazy = true },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+  keys = {
+    {
+      "<leader>d",
+      "<cmd>NvimTreeClose<cr><cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
+    },
+  },
+}
